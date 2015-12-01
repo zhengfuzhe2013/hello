d3aa263d07c06399cf9417dd1730ccdb51b053e4
yuyang <yuyang@kanzhun.com>
2015-12-01 16:22:12 +0800

    fix

---
+++ Hello/Hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello
+//
+//  Created by 于洋 on 15/12/1.
+//  Copyright © 2015年 yuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello/Hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello
+//
+//  Created by 于洋 on 15/12/1.
+//  Copyright © 2015年 yuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
